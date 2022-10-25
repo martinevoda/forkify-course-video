edb34cebf0954eb8b9ce102170899aa37086c417
martinevoda <martinevodanovic@gmail.com>
2022-10-25 11:23:04 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '48d3383d-6157-4ec9-a954-223a2c18acc3';
+export const MODAL_CLOSE_SEC = 2.5;
